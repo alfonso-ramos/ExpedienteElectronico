5b24d006418eb8e3b1751ed70576a9013dfc38fd
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2025-06-23 12:42:02 -0700

    [ADD] Modelo y repositorio de Consultas

--- src/main/java/rmp/expediente_electronico/modelo/Consulta.java
+++ src/main/java/rmp/expediente_electronico/modelo/Consulta.java
@@ -1,4 +1,30 @@
 package rmp.expediente_electronico.modelo;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class Consulta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idConsulta")
+    private Integer idPaciente;
+    private Integer edad;
+    private String diagnostico;
+    private String medicamentos;
+    private String observaciones;
+    private Integer talla;
+    private float altura;
+    private float peso;
+    private float imc;
+    @Temporal(TemporalType.DATE)
+    private Date fechaRegistro;
 }
